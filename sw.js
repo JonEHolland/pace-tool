6ce39c34a9f34d00359e68d383c4bb33bf970abb
Jon Holland <joneholland@gmail.com>
2025-12-28 20:06:13 -0800

    Updates

--- sw.js
+++ sw.js
@@ -1,30 +1,23 @@
 // Service Worker for offline PWA support
 // This will be automatically replaced with a unique hash during build
-const CACHE_NAME = 'pace-tool-29e53a20';
-
-console.log('[SW] Service worker loaded with cache:', CACHE_NAME);
+const CACHE_NAME = 'pace-tool-0060334b';
 
 self.addEventListener('install', (event) => {
   // Don't automatically skip waiting - let the app control when to update
   // Only skip waiting when user clicks "Update Now"
-  console.log('[SW] Installing new service worker with cache:', CACHE_NAME);
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('[SW] Activating service worker:', CACHE_NAME);
   event.waitUntil(
     caches.keys().then((cacheNames) => {
-      console.log('[SW] Found caches:', cacheNames);
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
-            console.log('[SW] Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
       );
     }).then(() => {
-      console.log('[SW] Notifying clients of activation');
       // Notify all clients that activation is complete
       return self.clients.matchAll().then((clients) => {
         clients.forEach((client) => {
@@ -38,9 +31,7 @@ self.addEventListener('activate', (event) => {
 
 // Listen for skip waiting message from the app
 self.addEventListener('message', (event) => {
-  console.log('[SW] Received message:', event.data);
   if (event.data && event.data.type === 'SKIP_WAITING') {
-    console.log('[SW] Skipping waiting...');
     self.skipWaiting();
   }
 });
